a4ace06e2edc72369ff633b5627660d8c52b8fe9
maria2332 <maria.ballesteros2004@gmail.com>
2022-11-03 19:58:54 +0100

    probando

---
+++ ejercicio1.py
@@ -0,0 +1,13 @@
+class alumno(): #Creamos la clase alumno
+    def _init_(self, nombre, nota):
+        self.nombre = nombre # Definimos que el atributo nombre, sera el nombre asignado
+        self.nota = nota # Definimos que el atributo nota, sera la nota asignada
+        print 
+    
+    def constructor(self):
+        alumno = ("Hola soy {} y mi nota es {}") #Mensaje
+        print(alumno.format(self.nombre, self.nota)) #Usamos FORMAT
+
+primero = alumno("Pedro", "10") #Instancia
+#Llamamos al método
+primero.constructor() 
